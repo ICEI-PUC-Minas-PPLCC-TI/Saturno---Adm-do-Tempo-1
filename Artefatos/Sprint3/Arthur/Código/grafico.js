a5e43d11bac4a4642cd1a442fb4cc87720f96a74
Rubens da Cunha Castro <89034366+RCastro13@users.noreply.github.com>
2021-11-30 20:25:01 -0300

    Add files via upload

---
+++ Artefatos/Sprint3/Arthur/Código/grafico.js
@@ -0,0 +1,57 @@
+let ctx = document.getElementById('container').getContext('2d');
+let labels =  ['Tarefas Concluidas', 'Tarefas a serem concluidas'];
+let colorHex = ['#43AA8B', '#FF0000', '#EFCA08'];
+
+let tarefasC;
+let tarefas;
+
+if(localStorage.getItem('quantidade') == null || localStorage.getItem('aConcluir') == null){
+  tarefasC = 1;
+  tarefas = 0;
+  localStorage.setItem('quantidade', tarefasC);
+  localStorage.setItem('aConcluir', tarefas);
+}else{
+  tarefasC = localStorage.getItem('quantidade');
+  tarefas = localStorage.getItem('aConcluir')
+}
+
+
+
+
+let myChart = new Chart(ctx, { 
+    type: 'pie',
+    data: {
+      datasets: [{
+        data: [`${tarefasC}`,`${tarefas}`],
+        backgroundColor: colorHex
+      }],
+      labels: labels
+    },
+    options: {
+      responsive: true,
+      legend: {
+        position: 'bottom'
+      },
+      plugins: {
+        datalabels: {
+          color: '#fff',
+          anchor: 'end',
+          align: 'start',
+          offset: -10,
+          borderWidth: 2,
+          borderColor: '#fff',
+          borderRadius: 25,
+          backgroundColor: (context) => {
+            return context.dataset.backgroundColor;
+          },
+          font: {
+            weight: 'bold',
+            size: '10'
+          },
+          formatter: (value) => {
+            return value + ' %';
+          }
+        }
+      }
+    }
+  })
---
+++ Artefatos/Sprint3/Arthur/Código/novaTarefa.js
@@ -0,0 +1,47 @@
+function leDados ()
+{
+    let strDados = localStorage.getItem('db');
+    let objDados = {};
+
+    if (strDados) 
+    {
+        objDados = JSON.parse (strDados);   
+    }
+    else 
+    {
+        objDados = { tarefas: [] }  
+    }
+
+    return objDados;
+} 
+
+function salvaDados (dados)
+{
+    localStorage.setItem ('db', JSON.stringify (dados));
+}
+
+function incluirTarefa ()
+{
+    let objDados = leDados();
+    let strNome = document.getElementById ('nomeTarefa').value;
+    let strDescricao = document.getElementById ('descricao').value;
+    let strDataLimite = document.getElementById ('datalimite').value;
+
+    let novaTarefa = 
+    {
+        nome: strNome,
+        dataLimite: strDataLimite,
+        descricao: strDescricao
+    }
+
+    //Código do grafico
+    let aConcluir = localStorage.getItem('aConcluir');
+    aConcluir++;
+    localStorage.setItem('aConcluir', aConcluir);
+
+    objDados.tarefas.push (novaTarefa);
+    salvaDados (objDados);
+}
+
+
+document.getElementById('criarTarefa').addEventListener('click', incluirTarefa);
